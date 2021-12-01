ad05c2081809a217a3f66429246e5289317f0bf3
Ivan Korniux <main@korniux.net>
2021-12-01 11:24:58 +0500

    Day 1, part 2, refactoring

--- 1/main.py
+++ 1/main.py
@@ -1,14 +1,15 @@
 def main():
   file = open('1/in.txt', 'r')
-  lines = file.readlines()
+  lines = [int(i) for i in file.readlines()]
   file.close()
 
-  result = 0
-  for i, val in enumerate(lines):
-    isIncreased = i != 0 and int(val) > int(lines[i - 1])
-    if isIncreased:
-      result += 1
-  
-  print(result)
+  triads = [sum(lines[i:i+3]) for i, _ in enumerate(lines)]
+
+  def calc(list_in):
+    is_inc = [i != 0 and val > list_in[i - 1] for i, val in enumerate(list_in)]
+    return is_inc.count(True)
+
+  print('Part 1: {}'.format(calc(lines)))
+  print('Part 2: {}'.format(calc(triads)))
 
 main()
